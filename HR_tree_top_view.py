af0bb61900535f737895a92f128879f627dcfefc
Soumya Jha <61748629+SoumyaJha813@users.noreply.github.com>
2024-11-20 22:45:47 +0530

    Create HR_tree_top_view.py

---
+++ HR_tree_top_view.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def topView(root):
+    #Write your code here
+    if root is None:
+        return
+    d = {}
+    def traverse(root, h, l):
+        if root is None:
+            return
+        if root:
+            if h not in d or d[h][1]>l:
+                d[h] = [root.info,l]
+        traverse(root.left,h-1,l+1)
+        traverse(root.right,h+1,l+1)
+    traverse(root,0,0)
+    
+    for h in sorted(d.keys()):
+        print(d[h][0], end = " ")
+        
+        
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+topView(tree.root)
